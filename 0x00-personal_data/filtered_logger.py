a24371e4cfeddbc648c4d92d648334635a0433b5
lujaja <jarzcyber@gmail.com>
2024-05-30 19:10:19 +0300

    db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -59,6 +59,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
         host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        db=os.getenv("PERSONAL_DATA_DB_NAME")
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
     )
     return db
