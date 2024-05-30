f54676ddb78572466f0b9548460655147fd21468
lujaja <jarzcyber@gmail.com>
2024-05-30 19:16:39 +0300

    encypty

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Encrypt password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Hash password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
