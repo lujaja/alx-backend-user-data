e2001692aabaef3b73a40eb43f6d00899a53693f
lujaja <jarzcyber@gmail.com>
2024-06-05 09:31:50 +0300

    paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,16 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Function require_auth"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ method authorized_header"""
