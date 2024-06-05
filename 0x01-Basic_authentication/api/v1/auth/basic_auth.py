e1b1dfe0930fb4f2c48e884285b88b7bc89dd05a
lujaja <jarzcyber@gmail.com>
2024-06-05 12:29:44 +0300

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,24 +6,36 @@ from api.v1.views import app_views
 from flask import request
 from flask.views import View, MethodView
 from models.user import User
+import base64
 
 
 class BasicAuth(Auth):
-    """ Basic Authentication Class """
+    """ Class BasicAuth
+    """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Method extract_base64_authorization_header
+        """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
-        """ Extract Base 64 Authorization Header """
-
-        if authorization_header is None:
+        if (
+            authorization_header is None
+            or type(authorization_header) is not str
+            or not authorization_header.startswith('Basic ')
+        ):
             return None
+        return authorization_header.split(' ')[1]
 
-        if not isinstance(authorization_header, str):
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """ Decode base64 authorization header """
+        if (
+            base64_authorization_header is None
+            or type(base64_authorization_header) is not str
+        ):
             return None
-
-        if not authorization_header.startswith("Basic "):
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
             return None
-
-        encoded = authorization_header.split(' ', 1)[1]
-
-        return encoded
