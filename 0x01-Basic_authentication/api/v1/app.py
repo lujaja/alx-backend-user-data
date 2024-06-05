59390d6ec28ebece47c981efaf98002d8a35b2e4
lujaja <jarzcyber@gmail.com>
2024-06-05 12:36:05 +0300

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -65,6 +65,7 @@ def forbidden(error) -> str:
 
 
 if __name__ == "__main__":
+    """ Main Function"""
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
