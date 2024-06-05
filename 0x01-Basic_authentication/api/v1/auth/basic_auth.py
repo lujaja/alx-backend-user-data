fb10930a5ffe2b115a05c8c88fc803c6b314d087
lujaja <jarzcyber@gmail.com>
2024-06-05 11:58:14 +0300

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,8 +12,7 @@ class BasicAuth(Auth):
     """Class BasicAuth"""
 
     def extract_base64_authorization_header(
-            self, authorization_header: str
-    ) -> str:
+            self, authorization_header: str) -> str:
         """Method extract_base64_authorization_header"""
         if (
             authorization_header is None
