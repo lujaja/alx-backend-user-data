d68c384253174b5cb68f0aaf3e5a681bb70fc86c
lujaja <jarzcyber@gmail.com>
2024-06-06 10:50:48 +0300

    user sesion

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,16 @@
 """ Empty Session
 """
 from api.v1.auth.auth import Auth
+from typing import Dict
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Define class SessionAuth"""
-    pass
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creats a Session id for user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        self.user_id_by_session_id[str(uuid4())] = user_id
