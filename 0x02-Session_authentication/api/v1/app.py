b53732c5bb07fdf80cd2ce00b059d38d72191ff9
lujaja <jarzcyber@gmail.com>
2024-06-06 11:40:38 +0300

    before requets

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,15 @@ def before_request() -> str:
     request_path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
     if auth.require_auth(request.path, request_path_list):
         request.current_user = auth.current_user(request)
-        if auth.authorization_header(request) is None:
+        if (
+            auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None
+        ):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
