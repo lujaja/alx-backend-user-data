802c742f29193896747f7e9c3a00d1df0fe57d58
lujaja <jarzcyber@gmail.com>
2024-06-06 10:57:10 +0300

    session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,7 +14,7 @@ class SessionAuth(Auth):
         if user_id is None or type(user_id) is not str:
             return None
 
-        session_id = str(uuid4)
+        session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
